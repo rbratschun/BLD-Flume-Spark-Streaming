38ef5b66d0399747fc1a379fce7f6c7095a1f197
Roman Bratschun <rbratschun@gmail.com>
2018-05-28 19:54:19 +0200

    close mysql connection after transaction

--- spark-streaming/src/main/java/at/mse/bld/spark/Transaction.java
+++ spark-streaming/src/main/java/at/mse/bld/spark/Transaction.java
@@ -26,6 +26,7 @@ public class Transaction implements VoidFunction<Iterator<ProductStatistics>> {
             while(it.hasNext()) {
                 save(it.next());
             }
+            conn.close();
         }
         catch(Exception e) {
             System.out.println(e);
